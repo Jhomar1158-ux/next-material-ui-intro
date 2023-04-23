29313f47d1432fb7ad7aa950e71c4d125493b341
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2023-04-23 13:18:45 -0500

    wip: ✨ PROJECT-1 add main layout with metadata

---
+++ componets/layouts/Layout.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import {FC} from 'react'
+
+
+interface Props {
+    children?: React.ReactNode
+    title: string
+}
+
+export const Layout: FC<Props> = ({children, title}) => {
+    return (
+        <>
+            <Head>
+                <title> { title || 'My Shop'} </title>
+                <meta name='author' content='Jhomar Astuyauri'/>
+                <meta name='description' content={`Tienda de productos | ${title}`}/>
+                <meta name='keywords' content={`${title}, Productos, producto, item, items, tienda, e-commerce, marketplace`}/>
+            </Head>
+            {/* Navbar */}
+            <main>
+                {children}
+            </main>
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Layout } from '@/componets/layouts'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function HomePage() {
   return (
-    <>
-      <h1>Hola mundo!!</h1>
-    </>
+    <Layout title={'My shop Jhomar'}>
+      <button>Hola</button>
+    </Layout>
   )
 }
